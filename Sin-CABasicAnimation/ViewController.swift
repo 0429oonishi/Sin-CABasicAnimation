4867834ea784f05809e5338feda0fb586529f239
0429oonishi <oonishireon110@gmail.com>
2021-08-18 21:36:00 +0900

    実験

--- Sin-CABasicAnimation/ViewController.swift
+++ Sin-CABasicAnimation/ViewController.swift
@@ -9,117 +9,105 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    @IBOutlet private weak var myView: UIView!
     @IBOutlet private weak var myTopView: UIView!
-    @IBOutlet private weak var waveCountLabel: UILabel!
-    @IBOutlet private weak var amplitudeLabel: UILabel!
-    @IBOutlet weak var amplitudeSlider: UISlider!
+    @IBOutlet private weak var myView: UIView!
+    @IBOutlet private weak var myTopView2: UIView!
+    @IBOutlet private weak var myView2: UIView!
+    @IBOutlet private weak var myTopView3: UIView!
+    @IBOutlet private weak var myView3: UIView!
     
-    private var verticalBorderView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
-    private var horizontalBorderView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
+    @IBOutlet weak var tableView: UITableView!
     private var initCount: Int?
-    
+    private var color: UIColor { .systemRed.withAlphaComponent(0.5) }
+    private var color2: UIColor { .systemPink.withAlphaComponent(0.3) }
+    private var color3: UIColor { .systemBlue.withAlphaComponent(0.2) }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+
         self.view.backgroundColor = .white
         myView.backgroundColor = .clear
-        myTopView.backgroundColor = .red
-        amplitudeSlider.value = amplitudeSlider.maximumValue
-        
+        myTopView.backgroundColor = color
+        myView2.backgroundColor = .clear
+        myTopView2.backgroundColor = color2
+        myView3.backgroundColor = .clear
+        myTopView3.backgroundColor = color3
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         if initCount == nil {
-            myView.cutToSinCurve(waveCount: 1, amplitude: 10)
+            myView.cutToSinCurve(waveCount: 1.2, amplitude: 10, color: color)
+            myView2.cutToSinCurve(waveCount: 2, amplitude: 7, color: color2)
+            myView3.cutToSinCurve(waveCount: 0.9, amplitude: 9, color: color3)
             initCount = 1
         }
-        myView.layer.borderWidth = 2
-        myView.layer.borderColor = UIColor.black.cgColor
-        setupBorderView()
         
     }
     
-    @IBAction private func waveCountSliderValueDidChanged(_ sender: UISlider) {
-        let value = Int(sender.value)
-        waveCountLabel.text = String(value)
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-    @IBAction private func amplitudeSliderValueDidChanged(_ sender: UISlider) {
-        let value = Int(sender.value)
-        amplitudeLabel.text = String(value)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = String(indexPath.row)
+        return cell!
     }
     
-    @IBAction private func goButtonDidTapped(_ sender: Any) {
-        myView.cutToSinCurve(waveCount: Double(waveCountLabel.text!)!,
-                             amplitude: Double(amplitudeLabel.text!)!)
-    }
-    
-    private func setupBorderView() {
-        self.view.addSubview(verticalBorderView)
-        NSLayoutConstraint.activate([
-            verticalBorderView.widthAnchor.constraint(equalToConstant: 2),
-            verticalBorderView.centerXAnchor.constraint(equalTo: myView.centerXAnchor),
-            verticalBorderView.topAnchor.constraint(equalTo: myView.topAnchor),
-            verticalBorderView.bottomAnchor.constraint(equalTo: myView.bottomAnchor)
-        ])
-        self.view.addSubview(horizontalBorderView)
-        NSLayoutConstraint.activate([
-            horizontalBorderView.heightAnchor.constraint(equalToConstant: 2),
-            horizontalBorderView.centerYAnchor.constraint(equalTo: myView.centerYAnchor),
-            horizontalBorderView.leadingAnchor.constraint(equalTo: myView.leadingAnchor),
-            horizontalBorderView.trailingAnchor.constraint(equalTo: myView.trailingAnchor)
-        ])
-    }
     
 }
 
 extension UIView {
     
-    func cutToSinCurve( waveCount: Double, amplitude: Double) {
+    func cutToSinCurve(waveCount: Double, amplitude: Double, color: UIColor) {
         let layer = CAShapeLayer()
         layer.frame = CGRect(x: 0,
                              y: 0,
                              width: self.frame.width,
                              height: self.frame.height)
-        layer.fillColor = UIColor.red.cgColor
-        layer.path = createSinPath(frame: layer.frame, waveCount: waveCount, amplitude: amplitude)
+        layer.fillColor = color.cgColor
+        layer.strokeColor = color.cgColor
+        layer.lineWidth = 3
+        layer.path = createSinPath(layer: layer, waveCount: waveCount, amplitude: amplitude)
         if let layer = self.layer.sublayers?.first {
             layer.removeFromSuperlayer()
         }
         self.layer.addSublayer(layer)
     }
     
-    private func createSinPath(frame: CGRect, waveCount: Double, amplitude: Double) -> CGPath {
+    private func createSinPath(layer: CAShapeLayer, waveCount: Double, amplitude: Double) -> CGPath {
         let div: Double = 1 / 100
         let sinPath = UIBezierPath()
-        let origin = CGPoint(x: 0,
-                             y: frame.height / 2)
+        let originX: CGFloat = layer.lineWidth
+        let origin = CGPoint(x: -originX,
+                             y: layer.frame.height / 2)
         let count = waveCount * 2
-        let xRatioToFill = Double(frame.width) / (Double.pi / div)
+        let xRatioToFill = Double(layer.frame.width) / (Double.pi / div)
         sinPath.move(to: CGPoint(x: origin.x, y: 0))
         sinPath.addLine(to: CGPoint(x: origin.x, y: origin.y))
         for i in 0...Int(Double.pi / div * count) {
             let x = div * Double(i)
             let y = sin(x)
-            sinPath.addLine(to: CGPoint(x: (x / div / count + Double(origin.x)) * xRatioToFill,
-                                        y: Double(origin.y) * (1 - y * amplitude / 10))
+            sinPath.addLine(to: CGPoint(x: (x / div / count + Double(origin.x)) * xRatioToFill + Double(originX * 2),
+                                        y: Double(origin.y) * (1 - y / amplitude))
             )
-            print(amplitude)
         }
-        sinPath.addLine(to: CGPoint(x: frame.width, y: 0))
+        sinPath.addLine(to: CGPoint(x: layer.frame.width + originX, y: 0))
         return sinPath.cgPath
     }
     
